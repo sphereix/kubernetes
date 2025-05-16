c7ee8ddc8c8aab61b548802456534e9837a50308
sphereix <45791069+sphereix@users.noreply.github.com>
2025-05-16 13:51:58 +0100

    Update wordpress.groovy

--- jenkinsfiles/wordpress.groovy
+++ jenkinsfiles/wordpress.groovy
@@ -1,39 +1,57 @@
 pipeline {
-    
-    agent any
+  agent {
+    kubernetes {
+      label 'jenkins-kubectl-agent'
+      yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    some-label: kubectl
+spec:
+  containers:
+    - name: jnlp
+      image: jenkins/inbound-agent:alpine
+      args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
+    - name: kubectl
+      image: bitnami/kubectl:latest
+      command:
+        - cat
+      tty: true
+"""
+      defaultContainer 'kubectl'
+    }
+  }
 
-        stages {
+  environment {
+    // Add your environment variables here if needed
+  }
 
-            stage('git checkout') {
-	
-                steps {
+  stages {
+    stage('git checkout') {
+      steps {
+        checkout([$class: 'GitSCM',
+          branches: [[name: '*/master']],
+          userRemoteConfigs: [[url: 'https://github.com/sugreevudu/kubernetes.git']]
+        ])
+      }
+    }
 
-	            checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/sugreevudu/kubernetes.git']]])			                   			                           
-                      }
-				   }
-								          
-            stage('mysql deployment') {
-			  steps {
-			    sh '''kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-user-pass.yaml
-	              kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-db-url.yaml
-                kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-root-pass.yaml
-                kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-pv.yaml
-                kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-pvc.yaml
-                kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-deployment.yaml
-                kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-service.yaml'''		         
-	                 }
-			    }
-          stage('php-myadmin-deployment') {
-			     steps {
-			          sh '''kubectl apply -f wordpre-phpmysql-mysql-deployments/phpmyadmin-deploy.yaml
-	              kubectl apply -f wordpre-phpmysql-mysql-deployments/phpmyadmin-service.yaml'''         
-	                 }
-			    }
-          stage('wordpress-deployment') {
-			     steps {
-			          sh '''kubectl apply -f wordpre-phpmysql-mysql-deployments/wordpress-deploy.yaml
-	              kubectl apply -f wordpre-phpmysql-mysql-deployments/wordpress-service.yaml'''         
-	                 }
-			    }
-          }
-	}
+    stage('mysql deployment') {
+      steps {
+        container('kubectl') {
+          sh '''
+            kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-user-pass.yaml
+            kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-db-url.yaml
+            kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-root-pass.yaml
+            kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-pv.yaml
+            kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-pvc.yaml
+            kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-deployment.yaml
+          '''
+        }
+      }
+    }
+
+    // Additional stages...
+  }
+}
